165fc131cc4f284cc4599b392e226e3632912045
HiImWhite <wojttasek1999@gmail.com>
2023-03-05 17:55:46 +0100

    Setting up frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
