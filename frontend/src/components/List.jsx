5b7483f345d71a2614c247424a232292f1a8bfc1
HiImWhite <wojttasek1999@gmail.com>
2023-03-06 13:03:52 +0100

    Update List.jsx

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styles from './List.module.css';
 
 const List = () => {
   const [movies, setMovies] = useState([]);
@@ -13,12 +14,43 @@ const List = () => {
   }, []);
 
   return (
-    <ul>
-      {movies.map((item) => (
-        <li>{item.name}</li>
-      ))}
-    </ul>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th className={styles.tableHeader}>Rank</th>
+          <th className={styles.tableHeader}>Name</th>
+          <th className={styles.tableHeader}>Year</th>
+          <th className={styles.tableHeader}>Rating</th>
+          <th className={styles.tableHeader}>Genre</th>
+          <th className={styles.tableHeader}>Certificate</th>
+          <th className={styles.tableHeader}>Run Time</th>
+          <th className={styles.tableHeader}>Tagline</th>
+          <th className={styles.tableHeader}>Budget</th>
+          <th className={styles.tableHeader}>Box Office</th>
+          <th className={styles.tableHeader}>Casts</th>
+          <th className={styles.tableHeader}>Directors</th>
+          <th className={styles.tableHeader}>Writers</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie.rank}>
+            <td>{movie.rank}</td>
+            <td>{movie.name}</td>
+            <td>{movie.year}</td>
+            <td>{movie.rating}</td>
+            <td>{movie.genre}</td>
+            <td>{movie.certificate}</td>
+            <td>{movie.run_time}</td>
+            <td>{movie.tagline}</td>
+            <td>{movie.budget}</td>
+            <td>{movie.box_office}</td>
+            <td>{movie.casts}</td>
+            <td>{movie.directors}</td>
+            <td>{movie.writers}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
-
-export default List;
