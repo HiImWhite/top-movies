13558074219dc12d08312800711ca7d49b459d67
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-06 11:57:26 +0100

    Create movieModel.js

---
+++ backend/models/movieModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  rank: Number,
+  name: String,
+  year: Number,
+  rating: String,
+  genre: String,
+  certificate: String,
+  run_time: String,
+  tagline: String,
+  budget: String,
+  box_office: String,
+  casts: String,
+  directors: String,
+  writers: String,
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
