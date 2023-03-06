cc00c812e61e9feda180534b93dcfdafbb652748
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-06 17:31:32 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import Table from './components/Table';
-import './App.css';
+import { useState } from 'react';
+import Table from './components/Table/Table';
+import StickyButton from './components/StickyButton/StickyButton';
+import Modal from './components/Modal/Modal';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className='App'>
       <Table />
+      <StickyButton
+        handleOpen={() => {
+          setOpenModal(true);
+        }}
+      />
+      <Modal open={openModal} handleClose={() => setOpenModal(false)} />
     </div>
   );
 }
