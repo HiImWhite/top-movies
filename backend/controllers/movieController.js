17e93792bfee64990706db70cd4958e301c42f72
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-06 16:21:56 +0100

    Update movieController.js

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -5,18 +5,31 @@ const start = (req, res, next) => {
     return res.status(200).json('Hello there');
   } catch (err) {
     console.log(err);
-    res.sendStatus(500);
+    res.sendStatus(400);
   }
 };
 
 const getAllMovies = async (req, res) => {
   try {
-    // const movies = await MovieModel.find({});
-    const filters = req.query;
-    const movies = await MovieModel.find(filters);
-    res.status(200).send(movies);
+    const params = req.query;
+    console.log(params);
+
+    if (!Object.keys(params).length) {
+      const movies = await MovieModel.find({});
+      res.status(200).send(movies);
+    } else {
+      const key = Object.keys(params);
+      const value = Object.values(params).pop();
+      const filters = {};
+
+      filters[key] = { $regex: value, $options: 'i' };
+      console.log(filters);
+
+      const movies = await MovieModel.find(filters);
+      res.status(200).send(movies);
+    }
   } catch (err) {
-    res.status(500).send(err);
+    res.status(400).send(err);
   }
 };
 
