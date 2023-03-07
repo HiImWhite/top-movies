6f64f751f99501a4228a7c3e604ed272823c9c97
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-07 11:59:26 +0100

    Fixed filter input handling. Added no data found info

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -21,44 +21,57 @@ const HeadTitles = [
 
 const Table = () => {
   const [movies, setMovies] = useState([]);
+  const [input, setInput] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getMovies();
       setMovies(response.data);
     };
+
     fetchData();
   }, []);
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {HeadTitles.map((title) => (
-            <TableHead key={title} title={title} handleState={setMovies} />
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map((movie) => (
-          <tr key={movie.rank}>
-            <td>{movie.rank}</td>
-            <td>{movie.name}</td>
-            <td>{movie.year}</td>
-            <td>{movie.rating}</td>
-            <td>{movie.genre}</td>
-            <td>{movie.certificate}</td>
-            <td>{movie.run_time}</td>
-            <td>{movie.tagline}</td>
-            <td>{movie.budget}</td>
-            <td>{movie.box_office}</td>
-            <td>{movie.casts}</td>
-            <td>{movie.directors}</td>
-            <td>{movie.writers}</td>
+    <>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {HeadTitles.map((title) => (
+              <TableHead
+                key={title}
+                title={title}
+                handleMovies={setMovies}
+                input={input}
+                handleInput={setInput}
+              />
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        {movies.length > 0 && (
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie.rank}>
+                <td>{movie.rank}</td>
+                <td>{movie.name}</td>
+                <td>{movie.year}</td>
+                <td>{movie.rating}</td>
+                <td>{movie.genre}</td>
+                <td>{movie.certificate}</td>
+                <td>{movie.run_time}</td>
+                <td>{movie.tagline}</td>
+                <td>{movie.budget}</td>
+                <td>{movie.box_office}</td>
+                <td>{movie.casts}</td>
+                <td>{movie.directors}</td>
+                <td>{movie.writers}</td>
+              </tr>
+            ))}
+          </tbody>
+        )}
+      </table>
+      {movies.length === 0 && <div className={styles.info}>No data found</div>}
+    </>
   );
 };
 
--- frontend/src/components/Table/TableHead.jsx
+++ frontend/src/components/Table/TableHead.jsx
@@ -3,29 +3,24 @@ import { getMovies } from '../../services/movieServices';
 import styles from './TableHead.module.css';
 
 const TableHead = (props) => {
-  const { title, handleState } = props;
+  const { title, handleMovies, input, handleInput } = props;
 
   const handleFilter = async (e) => {
+    const value = e.target.value;
+    handleInput({ [title]: value });
+
     try {
-      if (!e.target.value) {
+      if (!value) {
         const res = await getMovies();
-        handleState(res.data);
+        handleMovies(res.data);
         return;
       }
-
-      const params = {};
-      params[title.toLowerCase()] = e.target.value;
+      const params = { [title.toLowerCase()]: value };
       const res = await getMovies(params);
-      handleState(res.data);
+      handleMovies(res.data);
     } catch (err) {
       console.log(err);
     }
-  };
-
-  const handleClear = async (e) => {
-    e.target.value = '';
-    const res = await getMovies();
-    handleState(res.data);
   };
 
   return (
@@ -35,9 +30,9 @@ const TableHead = (props) => {
         <input
           className={styles.input}
           type='text'
-          onKeyUp={handleFilter}
-          onBlur={handleClear}
+          onChange={handleFilter}
           placeholder='Filter...'
+          value={input[title] || ''}
           title={`Type in a ${title}`}></input>
       </div>
     </th>
