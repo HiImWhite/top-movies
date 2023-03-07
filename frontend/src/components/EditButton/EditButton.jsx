4b16033f7445befd43e2fbe96838b6d8e3ac7514
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-07 12:24:27 +0100

    Created EditButton

---
+++ frontend/src/components/EditButton/EditButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EditIcon from '../Icons/EditIcon';
+import styles from './EditButton.module.css';
+
+const EditButton = () => {
+  return (
+    <div className={styles.icon}>
+      <EditIcon />
+    </div>
+  );
+};
+
+export default EditButton;
