f213eda09bb4c7bb88eefe8460f4be1ede2f67e9
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-07 12:24:16 +0100

    Changed icon

--- frontend/src/components/StickyButton/StickyButton.jsx
+++ frontend/src/components/StickyButton/StickyButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import AddIcon from '../Icons/AddIcon';
 import styles from './StickyButton.module.css';
 
 const StickyButton = (props) => {
-  return <div className={styles.icon} onClick={props.handleOpen} />;
+  return (
+    <div className={styles.icon} onClick={props.handleOpen}>
+      <AddIcon />
+    </div>
+  );
 };
 
 export default StickyButton;
