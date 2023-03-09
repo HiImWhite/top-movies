bc4c545234a84c799acf4ddc29f9e5d381789cc8
JagWoyat <jagwoyat@tutanota.com>
2023-03-09 18:26:12 +0100

    Modal fixes

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom';
 import CloseIcon from '../Icons/CloseIcon';
 import styles from './Modal.module.css';
 
-const Modal = (props) => {
-  if (!props.open) return null;
+const Modal = ({open, handleClose, title = "", children}) => {
+  if (!open) return null;
 
   return ReactDOM.createPortal(
     <div className={styles.overlay}>
-      <div className={styles.backdrop} onClick={props.handleClose}></div>
+      <div className={styles.backdrop} onClick={handleClose}></div>
       <div className={styles.dialog}>
         <div className={styles.content}>
           <div className={styles.nav}>
-            <h2>{props.title}</h2>
-            <button className={styles.icon} onClick={props.handleClose}>
+            <h2>{title}</h2>
+            <button className={styles.icon} onClick={handleClose}>
               <CloseIcon />
             </button>
           </div>
-          {props.children}
+          {children}
         </div>
       </div>
     </div>,
