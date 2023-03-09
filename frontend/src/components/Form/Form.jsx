8da29820a4118302b2c66b2cf40bf689aa8c46cc
HiImWhite <wojttasek1999@gmail.com>
2023-03-09 16:58:13 +0100

    Update Form.jsx

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,33 +1,41 @@
 import styles from './Form.module.css';
 
 const movieSchema = {
-    rank: String,
-    name: String,
-    year: String,
-    rating: String,
-    genre: String,
-    certificate: String,
-    run_time: String,
-    tagline: String,
-    budget: String,
-    box_office: String,
-    casts: String,
-    directors: String,
-    writers: String,
-  };
+  rank: String,
+  name: String,
+  year: String,
+  rating: String,
+  genre: String,
+  certificate: String,
+  run_time: String,
+  tagline: String,
+  budget: String,
+  box_office: String,
+  casts: String,
+  directors: String,
+  writers: String,
+};
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       {Object.keys(movieSchema).map((key, i) => (
-        <div>
-            <input
+        <div key={key} className={styles.textInputs}>
+          <input
             className={styles.input}
             type='text'
             placeholder={key}
             title={`Type in a ${key}`}></input>
         </div>
       ))}
+      <div className={styles.submitButton}>
+        <input className={styles.input} type='submit' value='Submit' />
+      </div>
     </form>
   );
 };
