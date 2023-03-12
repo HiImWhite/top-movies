d3cb84dff98c2521282becc19d5d7c09cb4c33d1
JagWoyat <jagwoyat@tutanota.com>
2023-03-12 17:19:00 +0100

    Added Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,39 @@
 import Table from './components/Table/Table';
 import StickyButton from './components/StickyButton/StickyButton';
-import Form from './components/Form/Form';
+import styles from './App.module.css';
+import { useState } from 'react';
+import Stats from './components/Stats/Stats';
 
 function App() {
+  const [isTable, setIsTable] = useState(false);
+
+  const SelectedTable = {
+    backgroundColor: isTable ? "#BBBBBB" : "#777777"
+  };
+
+  const SelectedStats = {
+    backgroundColor: isTable ? "#777777" : "#BBBBBB"
+  };
+
   return (
-    <div className='App'>
-      <Table />
-      <StickyButton />
-      {/* <Form /> */}
+    <div>
+      <div className={styles.Navbar}>
+        <ul className={styles.List}>
+          <li style={SelectedTable} onClick={() => {setIsTable(true)}}>Table</li>
+          <li style={SelectedStats} onClick={() => {setIsTable(false)}}>Stats</li>
+        </ul>
+      </div>
+      {
+        isTable ?
+        <>
+          <Table />
+          <StickyButton />
+        </>
+        :
+        <>
+          <Stats />
+        </>
+      }
     </div>
   );
 }
---
+++ frontend/src/components/Stats/Stats.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styles from './Stats.module.css';
+
+export default function Stats() {
+  return (
+    <div>
+        Stats
+    </div>
+  );
+};
