4003ec6641856e0e98e497eb0f268ebce3e654e8
HiImWhite <wojttasek1999@gmail.com>
2023-03-13 22:40:46 +0100

    Update Stats.jsx

--- frontend/src/components/Stats/Stats.jsx
+++ frontend/src/components/Stats/Stats.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { getStats } from '../../services/movieServices';
 import styles from './Stats.module.css';
 
-export default function Stats() {
+const Stats = () => {
   const [stats, setStats] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getStats("avgbox");
+        const response = await getStats('avgbox');
         setStats(response.data);
       } catch (err) {
         console.log(err);
@@ -17,17 +17,14 @@ export default function Stats() {
     fetchData();
   }, []);
 
-
   return (
     <div>
       <h1>Average box office of directors</h1>
-        {
-          Object.keys(stats).map((key) => (
-            <div>
-              {stats[key].movies > 1 && `${key}: ${stats[key].avgBox}$`}
-            </div>
-          ))
-        }
+      {Object.keys(stats).map((key) => (
+        <div>{stats[key].movies > 1 && `${key}: ${stats[key].avgBox}$`}</div>
+      ))}
     </div>
   );
 };
+
+export default Stats;
