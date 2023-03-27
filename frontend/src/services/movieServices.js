8b3c07612da1d514ade639bd06da53e35eac9ce6
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-27 22:35:11 +0200

    Update movieServices.js

--- frontend/src/services/movieServices.js
+++ frontend/src/services/movieServices.js
@@ -1,23 +1,24 @@
-import axios from "axios";
-const apiUrl = "http://localhost:8000";
+import axios from 'axios';
+// const apiUrl = "http://localhost:8000";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export function getMovies(params = null) {
   if (params)
-    return axios.get(apiUrl + "/movies", {
+    return axios.get(apiUrl + '/movies', {
       params: params,
     });
 
-  return axios.get(apiUrl + "/movies");
+  return axios.get(apiUrl + '/movies');
 }
 
 export function addMovie(movie) {
-  return axios.post(apiUrl + "/movies", movie);
+  return axios.post(apiUrl + '/movies', movie);
 }
 
 export function updateMovie(id, movie) {
-  return axios.put(apiUrl + "/movies/" + id, movie);
+  return axios.put(apiUrl + '/movies/' + id, movie);
 }
 
 export function getStats(stat) {
-  return axios.get(apiUrl + "/stats/" + stat);
+  return axios.get(apiUrl + '/stats/' + stat);
 }
