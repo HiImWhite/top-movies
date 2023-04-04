7381c2664692809960b88742cd09a357ffc0bf4c
Wojciech Bielawa <wojttasek1999@gmail.com>
2023-04-04 22:27:06 +0200

    Fix url

--- frontend/src/services/movieServices.js
+++ frontend/src/services/movieServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const apiUrl = "http://localhost:8000";
-const apiUrl = import.meta.env.VITE_API_URL;
+ const apiUrl = "http://localhost:8000";
+//const apiUrl = import.meta.env.VITE_API_URL;
 
 export function getMovies(params = null) {
   if (params)
